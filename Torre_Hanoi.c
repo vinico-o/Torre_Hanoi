6371cb800186d17f5e6fe5208f6df25239625912
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-05-02 17:12:02 -0300

    feat Vazia function

--- Torre_Hanoi.c
+++ Torre_Hanoi.c
@@ -1,7 +1,18 @@
 #include "Torre_Hanoi.h"
+#include <stdbool.h>
 
-void InicializarPilha(Pilha *pilha) {
+void InicializarPilha(Pilha *pilha)
+{
     pilha->topo = NULL;
     pilha->qntd = 0;
 }
 
+bool Vazia(Pilha *pilha)
+{
+    if(pilha->qntd == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
