a8eb1f147cb2158b06ba8271a073b4a0d2bf1225
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-05-03 15:14:00 -0300

    feat: add Menu function and fix reverse order in Imprimir

--- Torre_Hanoi.c
+++ Torre_Hanoi.c
@@ -21,32 +21,44 @@ bool Vazia(Pilha *pilha)
 
 void Imprimir(Pilha *pilha)
 {
+    //por conta do metodo de insercao, a impressao sera feita ao contrario, para melhor visualizacao da torre
     if(Vazia(pilha))
     {
         printf("Vazia");
+        return;
     }
-    
-    for(No* ptr = pilha->topo; ptr != NULL; ptr = ptr->prox)
+
+    Cores elementos[3];
+    int i = 0;
+
+    for(No* ptr = pilha->topo; ptr != NULL; ptr = ptr->prox, i++)
     {
-        printf("%d ", ptr->cor);
+        elementos[i] = ptr->cor;
+    }
+
+    while(i > 0)
+    {
+        printf("%d ", elementos[i - 1]);
+        i--;
     }
 }
 
-void ImprimirTorre(Pilha* origem, Pilha* auxiliar, Pilha* destino)
+void ImprimirTorre(Pilha pilha[])
 {
+    printf("\n");
     for(int i = 0; i < 7; i++)
     {
         printf("| ");
         switch (i)
         {
             case 1:
-                Imprimir(origem);
+                Imprimir(&pilha[0]);
                 break;
             case 3:
-                Imprimir(auxiliar);
+                Imprimir(&pilha[1]);
                 break;
             case 5:
-                Imprimir(destino);
+                Imprimir(&pilha[2]);
                 break;
         }
         printf("\n");
@@ -55,7 +67,30 @@ void ImprimirTorre(Pilha* origem, Pilha* auxiliar, Pilha* destino)
     printf("\n");
 }
 
-void Empilhar (Pilha *pilha, Cores disco)
+void Menu(Pilha pilha[])
+{
+    int tamanho = pilha[0].qntd;
+    int opcao1, opcao2;
+
+    ImprimirTorre(pilha);
+
+    do
+    {
+        printf("Digite a Torre de Origem: ");
+        scanf("%d", &opcao1);
+        printf("Digite a Torre de Destino: ");
+        scanf("%d", &opcao2);
+
+        TrocarPinos(&pilha[opcao1], &pilha[opcao2]);
+        ImprimirTorre(pilha);
+
+    } while (pilha[2].qntd != tamanho);
+
+    printf("Desafio Solucionado!\n");
+    
+}
+
+void Empilhar(Pilha *pilha, Cores disco)
 {
     No* novo = malloc(sizeof(No));
     if(novo == NULL)
