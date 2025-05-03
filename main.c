24fab5d3597c505b3c5b2d5d04f85b9df365ed24
Vinícius Mardegan <vinimardegan2006@gmail.com>
2025-05-03 15:19:45 -0300

    feat: implement complete game in main

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include "Torre_Hanoi.c"
+#define MAX 3
 
 int main() {
     
-    
+    Pilha pilha[MAX];
+
+    InicializarPilha(&pilha[0]);
+    InicializarPilha(&pilha[1]);
+    InicializarPilha(&pilha[2]);
+
+    Empilhar(&pilha[0], VERMELHO);
+    Empilhar(&pilha[0], VERDE);
+    Empilhar(&pilha[0], AZUL);
+
+    Menu(pilha);
+
     return 0;
 }
